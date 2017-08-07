0e323174af4e898a87ee57ba3a4a0df39cf584e4
rezaf <reza.farajian@gmail.com>
2017-08-07 00:06:40 -0400

    Setup route with cross origin requests enabled, sending test message

--- chatmunk.rb
+++ chatmunk.rb
@@ -1,3 +1,18 @@
 require 'sinatra'
 require 'sinatra/cross_origin'
 require 'net/http'
+
+register Sinatra::CrossOrigin
+
+post '/chat/messages' do
+  cross_origin
+
+  {
+    messages: [
+      {
+        type: 'text',
+        text: 'HELLO!'
+      }
+    ]
+  }.to_json
+end
