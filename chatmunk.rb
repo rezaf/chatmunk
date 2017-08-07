13516d634127528bb9a88f8d60fdc436194f1234
rezaf <reza.farajian@gmail.com>
2017-08-07 00:39:53 -0400

    Parse question, and extract location string

--- chatmunk.rb
+++ chatmunk.rb
@@ -11,7 +11,7 @@ post '/chat/messages' do
     if params[:action] == 'join'
       "Hello, #{params[:name]}!"
     else
-      "In progress."
+      parse_question
     end
 
   {
@@ -22,4 +22,29 @@ post '/chat/messages' do
       }
     ]
   }.to_json
+end
+
+def parse_question
+  question = params[:text]
+
+  location =
+    if question[0..20]&.downcase == "what's the weather in"
+      question[21..-1]
+    elsif question[0..9]&.downcase == 'weather in'
+      question[10..-1]
+    elsif question[-7..-1]&.downcase == 'weather'
+      question[0..-8]
+    else
+      ''
+    end.strip
+
+  if location.empty?
+    'Hmm, I am not sure if I understand this question.'
+  else
+    weather(location)
+  end
+end
+
+def weather(location)
+  'In progress!'
 end
