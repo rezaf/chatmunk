58c400e0a99de798430d5d11619b8ea419253569
rezaf <reza.farajian@gmail.com>
2017-08-11 17:17:44 -0400

    Add spec for testing API-hitting method, stub requests with Webmock

--- spec/chatmunk_spec.rb
+++ spec/chatmunk_spec.rb
@@ -15,8 +15,50 @@ RSpec.describe 'Chatmunk Chat Engine' do
 
   context 'when action is message' do
     context 'when weather question is in right format' do
-      xit 'responds with weather info' do
+      it 'responds with correct weather info' do
+        stub_request(
+          :get,
+          /maps\.googleapis\.com\/maps\/api\/geocode\/json\?address=SF/
+        ).to_return(
+          status: 200,
+          body: {
+            "results" => [
+              {
+                "geometry" => {
+                  "location" => { "lat" => 37.7749295, "lng" => -122.4194155 }
+                }
+              }
+            ]
+          }.to_json
+        )
 
+        stub_request(
+          :get,
+          /api\.darksky\.net\/forecast\/.+\/37\.7749295,-122\.4194155/
+        ).to_return(
+          status: 200,
+          body: {
+            "currently" => {
+              "summary" => "Mostly Cloudy",
+              "temperature" => 58.29
+            }
+          }.to_json
+        )
+
+        json = {user_id: '1', action: 'message', text: 'SF weather'}
+
+        post '/chat/messages', json, {CONTENT_TYPE: 'application/json'}
+
+        expect(JSON.parse(last_response.body)).to eq(
+          {
+            'messages' => [
+              {
+                'type' => 'text',
+                'text' => 'Currently it is 58.29F. Mostly Cloudy'
+              }
+            ]
+          }
+        )
       end
     end
 
